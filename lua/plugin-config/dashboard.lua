18f4dd0749901c8ff55e494046da09ce2fb95b06
Zen <kefengong@gmail.com>
2022-11-02 18:13:11 +0800

    chore: update dashboard

--- lua/plugin-config/dashboard.lua
+++ lua/plugin-config/dashboard.lua
@@ -1,22 +1,10 @@
-vim.g.dashboard_default_executive = "telescope"
-vim.g.dashboard_custom_footer = { "" }
+local db = require('dashboard')
 
-vim.g.dashboard_custom_section = {
-  a = { description = { "  Projects              " }, command = "Telescope projects" },
-  b = { description = { "  Recently files        " }, command = "Telescope oldfiles" },
-  c = { description = { "  Edit keybindings      " }, command = "edit ~/.config/nvim/lua/keybindings.lua" },
-  d = {
-    description = { "  Edit Projects         " },
-    command = "edit ~/.local/share/nvim/project_nvim/project_history",
-  },
-  -- e = { description = { "  Edit .bashrc          " }, command = "edit ~/.bashrc" },
-  -- f = { description = { "  Change colorscheme    " }, command = "ChangeColorScheme" },
-  -- g = { description = { "  Edit init.lua         " }, command = "edit ~/.config/nvim/init.lua" },
-  -- h = { description = {'  Find file          '}, command = 'Telescope find_files'},
-  -- i = { description = {'  Find text          '}, command = 'Telescope live_grep'},
-}
 
-vim.g.dashboard_custom_header = {
+db.custom_footer = { "" }
+
+
+db.custom_header = {
   [[          ▀████▀▄▄              ▄█ ]],
   [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
   [[    ▄        █          ▀▀▀▀▄  ▄▀  ]],
@@ -26,4 +14,19 @@ vim.g.dashboard_custom_header = {
   [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ]],
   [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ]],
   [[   █   █  █      ▄▄           ▄▀   ]],
+}
+
+db.custom_center = {
+  {
+    desc = '  Projects              ',
+    action ='Telescope projects'
+  },
+  {
+    desc = '  Recently files        ',
+    action ='Telescope oldfiles'
+  },
+  {
+    desc = '  Edit keybindings      ',
+    action ='edit ~/.config/nvim/lua/keybindings.lua'
+  },
 }
